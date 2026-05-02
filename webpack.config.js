4d932708dedc9da05e09163bf855a3b383e862b0
Qi.Wang <gavin_qw@126.com>
2026-05-02 17:02:12 +0800

    修复 webpack 配置警告

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/js/main.js',
   output: {
     filename: 'bundle.js',
@@ -29,5 +30,8 @@ module.exports = {
     static: './dist',
     open: true,
     hot: true,
+  },
+  performance: {
+    hints: false,
   },
 };
